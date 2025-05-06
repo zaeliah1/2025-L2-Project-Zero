9b1a8701c2ae26d3b2c66704d6abcef2ab8abb2b
zaeliah1 <zaemasina@mmc.school.nz>
2025-05-07 10:57:12 +1200

    Flexible multi-line statement generator component

---
+++ C_01_Make_Statement_v2.py
@@ -0,0 +1,23 @@
+# Functions go here
+def make_statement(statement, decoration, lines):
+    """Creates headings (3 lines), subheadings (2 lines) and
+    emphasised text / mini-headings (1 line). Only use emoji for
+    single line statements"""
+
+    middle = f"{decoration * 3} {statement} {decoration * 3}"
+    top_bottom = decoration * len(middle)
+
+    if lines == 1:
+        print(middle)
+    elif lines == 2:
+        print(middle)
+        print(top_bottom)
+
+    else:
+        print(top_bottom)
+        print(middle)
+        print(top_bottom)
+# Main Routine goes here
+make_statement("Programming is Fun!", "👍", 1)
+make_statement("Programming is Still Fun!", "+", 6)
+make_statement("Emoji in Action", "#", 5)
---
+++ ZZ_Sandpit.py
@@ -0,0 +1,14 @@
+# reminder - here is an integer that has been multiplied by 3
+number = 5
+print(number * 3)
+
+# here is a string that has been multiplied by 3
+# (note the speech marks and text colour)
+num_string = "5"
+print(num_string * 3)
+
+# An example showing what the len(foo) method does
+example_text = "hello world"
+text_length = len(example_text)
+
+print(f"'{example_text}' is {text_length} characters long")
